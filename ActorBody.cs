e32a4ba6480c1d7370a8c8e81bb8fa710ee367bd
AnAdisorn <64987073+HeroAdisorn@users.noreply.github.com>
2024-03-13 00:38:35 +0100

    Add files via upload

---
+++ ActorBody.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public partial class ActorBody : CharacterBody3D
+{
+    public float Health { set; get; } = 100.0f;
+    public float Speed { set; get; } = 5.0f;
+    public float RunFactor { set; get; } = 2.0f;
+    public float Acceleration { set; get; } = 50.0f;
+    public float BrakeAcceleration { set; get; } = 50.0f;
+    public float JumpImpulse { set; get; } = 4.5f;
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+}
